735d95e4f2b7e8ba5a045d9105abd8a6d10e0864
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-19 17:19:15 -0600

    ?????

--- test/example.test.js
+++ test/example.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // IMPORT MODULES under test here:
 import { renderCarDetails } from '..render.js';
 
@@ -6,7 +7,7 @@ const test = QUnit.test;
 test('renderCarDetail test', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="plane-detail"><h2 class="type">Jumbo Jet.</h2><img src="../assets/1.png"><p class="speed">Medium speed intercontinental distance.</p><p class="size">Large.</p></div>`;
+    const expected = `<div class="car-info-container"><img src="../assets/1.jpg"><p class="brand">Honda</p><p class="model">Accord</p><p class="year">2009</p></div>`;
 
     //Act 
     // Call the function you're testing and set the result to a const
@@ -15,8 +16,6 @@ test('renderCarDetail test', (expect) => {
         brand: 'Honda',
         model: 'Accord',
         year: '2009',
-
-
     });
 
     //Expect
